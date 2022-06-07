70495cad334574677cb0329478dee2b17a4ce2b7
NoraPOM <norozco@proteccion.com.co>
2022-06-07 07:56:20 -0500

    first commit

---
+++ Matrixdiagonal.py
@@ -0,0 +1,56 @@
+# print a matrix
+
+def print_matrix(matrix):
+
+    for row in matrix:
+
+        temp = "["
+
+        for item in row:
+
+            temp+= str(item) + ","
+
+        print(temp[:-1]+"]")
+
+       
+
+
+def create_matrix(size):
+
+    matrix = []
+
+    for i in range(size):
+
+        row = []
+
+        for j in range(size):
+
+            if j == i:
+            
+                row.append(1)
+
+            elif (j + i == size - 1):
+
+                row.append(1)         
+        
+            else:
+    
+                row.append(0)
+
+        matrix.append(row)           
+
+    print_matrix(matrix)
+
+   
+
+def main():
+
+    size = input("Ingrese el numero: ")
+
+    create_matrix(int(size))
+
+
+
+if __name__ == "__main__":
+
+    main()
